6c6551d1ee9b1933c637cb92cb3b267c72a9d28b
moveuk <v_donguk@naver.com>
2024-03-05 00:28:43 +0900

    ZRR-55 Add: 공통 예외 처리 핸들러 수정 및 성공 응답시 반환 작성
    1. ErrorCode - code Type Long에서 Int로 변경
    2. ErrorResponse - code Type Long에서 Int로 변경 및 Generic 추가, 정적 팩토리 메서드 of 수정
    3. GlobalExceptionHandler - responseEntity ApiHelper 추가로 인한 변경
    4. ApiHelper - Response 반환 규격 통일 및 정리
    5. CommonResponse - 성공 응답에 대한 반환 타입 정의

--- src/main/kotlin/kr/zziririt/zziririt/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/kr/zziririt/zziririt/global/exception/GlobalExceptionHandler.kt
@@ -1,7 +1,7 @@
 package kr.zziririt.zziririt.global.exception
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kr.zziririt.zziririt.global.exception.dto.ErrorResponse
+import kr.zziririt.zziririt.global.responseEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
@@ -16,13 +16,9 @@ private val kLogger = KotlinLogging.logger {}
 @RestControllerAdvice
 class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
     @ExceptionHandler(CustomException::class)
-    fun handleCustomExceptionHandler(e: CustomException): ResponseEntity<Any>? {
+    fun handleCustomExceptionHandler(e: CustomException): ResponseEntity<Any> {
         kLogger.warn { "handleCustomExceptionHandler: ${e.errorCode.code}-${e.errorCode.message}" }
-        return if (e.message == null) {
-            ResponseEntity.status(e.errorCode.httpStatus).body(ErrorResponse.from(e.errorCode))
-        } else {
-            ResponseEntity.status(e.errorCode.httpStatus).body(ErrorResponse.from(e.errorCode, e.message))
-        }
+        return responseEntity(e)
     }
 
     override fun handleMethodArgumentNotValid(
@@ -34,7 +30,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
         val errorCode = ErrorCode.VALIDATION
         kLogger.warn { "handleMethodArgumentNotValid: ${errorCode.code}-${errorCode.message}" }
 
-        return ResponseEntity.status(errorCode.httpStatus).body(ErrorResponse.from(errorCode, e.getErrorMap()))
+        return responseEntity(errorCode, e.getErrorMap())
     }
 
     private fun MethodArgumentNotValidException.getErrorMap(): Map<String, String?> =
--- src/main/kotlin/kr/zziririt/zziririt/global/exception/dto/ErrorResponse.kt
+++ src/main/kotlin/kr/zziririt/zziririt/global/exception/dto/ErrorResponse.kt
@@ -3,30 +3,33 @@ package kr.zziririt.zziririt.global.exception.dto
 import com.fasterxml.jackson.annotation.JsonInclude
 import kr.zziririt.zziririt.global.exception.ErrorCode
 
-data class ErrorResponse(
-	val code: Long,
-	val message: String,
-	@JsonInclude(JsonInclude.Include.NON_EMPTY)
-	val payload: Any? = null
+data class ErrorResponse<T>(
+    val code: Int,
+    val message: String,
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    val payload: T? = null
 ) {
-	companion object {
-		fun from(errorCode: ErrorCode) : ErrorResponse = ErrorResponse(
-			code = errorCode.code,
-			message = errorCode.message
-		)
-		fun from(errorCode: ErrorCode, message: String) : ErrorResponse = ErrorResponse(
-			code = errorCode.code,
-			message = message
-		)
-		fun from(errorCode: ErrorCode, payload: Any?) : ErrorResponse = ErrorResponse(
-			code = errorCode.code,
-			message = errorCode.message,
-			payload = payload
-		)
-		fun from(errorCode: ErrorCode, message: String, payload: Any?) : ErrorResponse = ErrorResponse(
-			code = errorCode.code,
-			message = message,
-			payload = payload
-		)
-	}
+    companion object {
+        fun of(errorCode: ErrorCode): ErrorResponse<Nothing> = ErrorResponse(
+            code = errorCode.code,
+            message = errorCode.message
+        )
+
+        fun of(errorCode: ErrorCode, message: String): ErrorResponse<Nothing> = ErrorResponse(
+            code = errorCode.code,
+            message = message
+        )
+
+        fun <T> of(errorCode: ErrorCode, payload: T) = ErrorResponse(
+            code = errorCode.code,
+            message = errorCode.message,
+            payload = payload
+        )
+
+        fun <T> of(errorCode: ErrorCode, message: String, payload: T) = ErrorResponse(
+            code = errorCode.code,
+            message = message,
+            payload = payload
+        )
+    }
 }
