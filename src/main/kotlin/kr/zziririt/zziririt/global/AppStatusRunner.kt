ab95eb4928b332655f6396004a6f4b7690b50d55
moveuk <v_donguk@naver.com>
2024-03-05 12:29:31 +0900

    ZRR-21 Add: Application.yml 분리 및 Server 시작 상태 확인을 위한 AppRunner 작성
    1. Application.yml 분리
        - local, dev, prod: 3 level
    2. Server 시작 상태 로깅을 위한 AppStatusRunner 작성

---
+++ src/main/kotlin/kr/zziririt/zziririt/global/AppStatusRunner.kt
@@ -0,0 +1,24 @@
+package kr.zziririt.zziririt.global
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.core.env.Environment
+import org.springframework.stereotype.Component
+
+private val kLogger = KotlinLogging.logger {}
+@Component
+class AppStatusRunner(private val environment: Environment) : ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        kLogger.info { "===================다중 프로파일 테스트===================" }
+        kLogger.info { "Active profiles : ${environment.activeProfiles.toList()}" }
+        kLogger.info { "Datasource driver : ${environment.getProperty("spring.datasource.driver-class-name")}" }
+        kLogger.info { "Datasource url : ${environment.getProperty("spring.datasource.url")}" }
+        kLogger.info { "Datasource username : ${environment.getProperty("spring.datasource.username")}" }
+        kLogger.info { "Datasource password : ${environment.getProperty("spring.datasource.password")}" }
+        kLogger.info { "Server Port : ${environment.getProperty("server.port")}" }
+        kLogger.info { "Default Property : ${environment.getProperty("default.string")}" }
+        kLogger.info { "Common Property : ${environment.getProperty("common.string")}" }
+        kLogger.info { "====================================================" }
+    }
+}
