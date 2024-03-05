4e0de613c7f914730b57da6aa6884129de1c17b2
t2023-m0027 <y974348@gmail.com>
2024-03-05 20:50:35 +0900

    ZRR-52 Feat: FAQ Update API 개발
    1.FaqController:updateFaq 코드 작성.
    2.FaqService:updateFaq 코드 작성.
    3.UpdateFaqRequest:dto 작성.

--- src/main/kotlin/kr/zziririt/zziririt/api/faq/service/FaqService.kt
+++ src/main/kotlin/kr/zziririt/zziririt/api/faq/service/FaqService.kt
@@ -3,7 +3,11 @@ package kr.zziririt.zziririt.api.faq.service
 import jakarta.transaction.Transactional
 import kr.zziririt.zziririt.api.faq.dto.CreateFaqRequest
 import kr.zziririt.zziririt.api.faq.dto.FaqResponse
+import kr.zziririt.zziririt.api.faq.dto.UpdateFaqRequest
 import kr.zziririt.zziririt.domain.faq.repository.FaqRepository
+import kr.zziririt.zziririt.global.exception.ErrorCode
+import kr.zziririt.zziririt.global.exception.ModelNotFoundException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -15,5 +19,16 @@ class FaqService(
         val newFaqEntity = createFaqRequest.toFaqEntity()
         val savedFaqEntity = faqRepository.save(newFaqEntity)
         return FaqResponse.from(faqEntity = savedFaqEntity)
+    }
+
+    @Transactional
+    fun updateFaq(faqId: Long, updateFaqRequest: UpdateFaqRequest): FaqResponse {
+        val faqEntity = faqRepository.findByIdOrNull(faqId)
+            ?: throw ModelNotFoundException(ErrorCode.MODEL_NOT_FOUND, "FAQ not found")
+
+        faqEntity.question = updateFaqRequest.question
+        faqEntity.answer = updateFaqRequest.answer
+        val updatedFaqEntity = faqRepository.save(faqEntity)
+        return FaqResponse.from(updatedFaqEntity)
     }
 }
