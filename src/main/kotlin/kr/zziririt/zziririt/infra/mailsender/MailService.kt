e601f27b85e2e927c593441710e49ea92f388d50
doojoo9999 <96476393+doojoo9999@users.noreply.github.com>
2024-04-01 14:33:33 +0900

    ZRR-107 Bug: MailSender Bug Fix (#130)
    1. MimeMessageHelper 사용
    2. addRecipients -> setTo 수정

--- src/main/kotlin/kr/zziririt/zziririt/infra/mailsender/MailService.kt
+++ src/main/kotlin/kr/zziririt/zziririt/infra/mailsender/MailService.kt
@@ -1,7 +1,7 @@
 package kr.zziririt.zziririt.infra.mailsender
 
-import jakarta.mail.Message
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,20 +10,20 @@ class MailService(
 ) : MailClient {
 
     val message = javaMailSender.createMimeMessage()
+    val helper = MimeMessageHelper(message, true, "UTF-8")
     val zziririt = "cs@zziririt.kr"
 
     override fun sendMailToNewbie(recipient: String, nickname: String, createdAt: String) {
 
-        message.addRecipients(Message.RecipientType.TO, recipient)
-        message.setSubject("[zziririt] 회원 가입이 완료되었습니다.")
+        helper.setTo(recipient)
+        helper.setSubject("[zziririt] 회원 가입이 완료되었습니다.")
         val msg = """
             <h1>[회원가입 정보]</h1> <br>
             <h2> 닉네임: $nickname <br>
             <h2> 가입일: $createdAt
         """.trimIndent()
-        message.setText(msg, "UTF-8", "HTML")
-        message.setFrom(zziririt)
+        helper.setText(msg, true)
+        helper.setFrom(zziririt)
         javaMailSender.send(message)
-
     }
 }
